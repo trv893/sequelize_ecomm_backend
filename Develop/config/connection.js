820fa33194bac37ef941ef0171bf0cb14a5cffa3
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-14 11:59:14 -0400

    finilizes project with readme

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-
+// connects sequelize to the db through the .env file to obscure usn, db pw and db name
 const Sequelize = require('sequelize');
-
+console.log(process.env.DB_NAME);
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
